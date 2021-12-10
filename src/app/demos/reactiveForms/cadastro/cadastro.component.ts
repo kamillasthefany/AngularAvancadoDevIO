9712c3bb95fa51f9e3eb4adf6bd7a5ac001c003f
Kamilla <kamilla.sthefany@outlook.com>
2021-12-10 14:48:58 -0300

    Validação customizada de formulários

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgBrazilValidators } from 'ng-brazil';
+import { utilsBr } from 'js-brasil';
 import { Usuario } from './models/usuario';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,15 +14,22 @@ export class CadastroComponent implements OnInit {
   cadastroForm: FormGroup;
   usuario: Usuario;
   formResult: string = '';
+  MASKS = utilsBr.MASKS;
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([5, 10])]);
+    let senhaConfirmacao = new FormControl('', [Validators.required, CustomValidators.rangeLength([5, 10])], CustomValidators.equalTo(senha));
+
     this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required],
-      cpf: [''],
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(150)]],
+      cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
       email: ['', [Validators.required, Validators.email]],
-      senha: [''],
-      confirmacaoSenha: ['']
+      //Validação simples
+      //senha: ['', CustomValidators.rangeLength([5,10])],
+      senha: senha,
+      confirmacaoSenha: senhaConfirmacao
     });
   }
 
