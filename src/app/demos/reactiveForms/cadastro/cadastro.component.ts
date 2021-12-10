5f7df54a5664f883bbe4ec0753dc1c4be8600366
Kamilla <kamilla.sthefany@outlook.com>
2021-12-10 10:20:57 -0300

    Validação simples de formulários

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -10,20 +10,24 @@ import { Usuario } from './models/usuario';
 export class CadastroComponent implements OnInit {
   cadastroForm: FormGroup;
   usuario: Usuario;
+  formResult: string = '';
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
       nome: ['', Validators.required],
       cpf: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]],
       senha: [''],
       confirmacaoSenha: ['']
     });
   }
 
   adicionarUsuario() {
-    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.formResult = JSON.stringify(this.cadastroForm.value);
+    }
   }
 
 }
